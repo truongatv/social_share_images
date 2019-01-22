b8d51577890caa70aa4d8dad7456365267475d72
truongatv <truongatv2211@gmail.com>
2019-01-22 15:54:12 +0900

    add mongodb to project

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
 config = require('./server/configure');
-var app = express();
+
+var app = express(),
+	mongoose = require('mongoose');
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
+mongoose.connect('mongodb://localhost/imgShare');
+mongoose.connection.on('open', function(){
+	console.log('mongoose connected !');
+})
 app = config(app);
 var server = app.listen(app.get('port'), function(){
 	console.log('Server up : http://localhost:' + app.get('port'));
