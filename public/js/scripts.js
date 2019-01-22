37755bfa39299f05947e98c46dd7c97b4c45c9f5
truongatv <truongatv2211@gmail.com>
2019-01-22 17:42:47 +0900

    update CRUD with mongodb

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,3 +7,13 @@ $(function(){
 		$('#post-comment').show();
 	})
 });
+
+$('#btn-like').on('click', function(event) {
+	event.preventDefault();
+	console.log('test');
+	var imgId = $(this).data('id');
+
+	$.post('/images/'+ imgId + '/like').done(function(data) {
+		$('.likes-count').text(data.likes);
+	})
+})
